98328683a2dc11962968686469c52baf412e3b46
eliel <eli.levyarieli@gmail.com>
2024-10-26 15:33:00 +0300

    היי ניב סיימתי

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -6,19 +6,49 @@
 
 // Leave this import as it is. You'll need it
 import assignmentfiles.*;
-
 public class GameEngine {
+    private String targetWord;
+    private int attemptsLeft;
+    private boolean isWin;
 
     public GameEngine(String targetWord) {
-        
+        this.targetWord=targetWord;
+        this.attemptsLeft=6;
+        this.isWin=false;
     }
 
     public String playGuess(String guess) {
-        return "-----";
+        if (guess.equals(targetWord)) {
+            isWin = true;
+        }
+        if (this.attemptsLeft>0) {
+            this.attemptsLeft--;
+            return evaluateGuess(targetWord, guess);
+        } else {
+            return "No attempts left.";
+        }
     }
 
     public static void main(String[] args) {
-        
+        WordLoader wordLoader = new WordLoader();
+        String targetWord = wordLoader.getRandomWord();
+
+        GameEngine gameEngine = new GameEngine(targetWord);
+        GameUI gameUI = new GameUI();
+        System.out.println(targetWord);
+        String userGuess=" ";
+        while(!gameEngine.isGameOver()){
+            userGuess = gameUI.readUserGuess();
+            String feedback = gameEngine.playGuess(userGuess);
+            gameUI.displayResult(feedback, userGuess, gameEngine.getAttemptsLeft());
+            if (userGuess.equals(targetWord)) {
+                gameUI.displayWin();
+                gameEngine.attemptsLeft=0;
+            }
+        }
+        if (gameEngine.isGameOver() && !userGuess.equals(targetWord)) {
+            gameUI.displayLoss(gameEngine.getTargetWord());
+        }
     }
 
     /**
@@ -27,25 +57,37 @@ public class GameEngine {
      * - '*' if the character is in the correct position.
      * - '+' if the character is in the target word but in the wrong position.
      * - '-' if the character is not in the target word.
-     *
      * The comparison is done up to the length of the shorter word, ignoring any extra characters.
      * Example:
      * targetWord = "taper", guess = "water"
      * Returns: "-*+**"
      **/
     public static String evaluateGuess(String targetWord, String guess) {
-        return "-----";
+        String res = "";
+        for (int i = 0; i < targetWord.length() && i < guess.length(); i++) {
+            if (guess.charAt(i) == targetWord.charAt(i)) {
+                res += "*";
+            } else if (targetWord.indexOf(guess.charAt(i)) != -1) {
+                res += "+";
+            } else {
+                res += "-";
+            }
+        }
+            return res;
     }
 
     public boolean isGameOver() {
-        return false;
+        return isWin || attemptsLeft <= 0;
     }
 
     public boolean isWin() {
-        return false;
+        return isWin;
     }
 
+    public String getTargetWord() {
+        return targetWord;
+    }
     public int getAttemptsLeft() {
-        return 0;
+        return attemptsLeft;
     }
 }
--- src/GameUI.java
+++ src/GameUI.java
@@ -1,4 +1,22 @@
-
+import java.util.Scanner;
 public class GameUI {
-    
+    private Scanner input;
+
+    public GameUI() {
+        input = new Scanner(System.in);
+    }
+    public String readUserGuess(){
+        return input.nextLine();
+    }
+    public void displayResult(String res,String guess, int guessLeft){
+        System.out.println("You have " + guessLeft + " attempts left.");
+        System.out.println("Your guess: " + guess);
+        System.out.println("Feedback: " + res);
+    }
+    public void displayWin(){
+        System.out.println("Congratulations, YOU WIN!");
+    }
+    public void displayLoss(String targetWord){
+        System.out.println("Game over! The word was: " + targetWord);
+    }
 }
